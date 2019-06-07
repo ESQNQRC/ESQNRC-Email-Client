a2212f7e0905aeabf5ba7b8ca36fb87fb5de26f4
Daniel Berbesi <50345402+Berbex@users.noreply.github.com>
2019-06-06 20:08:01 -0400

    Update imapv1.py

--- imapv1.py
+++ imapv1.py
@@ -1,12 +1,11 @@
-import email, getpass, imaplib, os, time
-import threading
+import email, getpass, imaplib, os, time, threading, configparser, string, sys
 
 
 #################################################################################################################
 #               Team: Lambder                                                                                   #
 #                   Cesar Salazar                                                                               #
 #                   Daniel Berbesi                                                                              #
-#                   Saul                                                                                        #
+#                   Saul Ugueto                                                                                 #
 #                   Valentina Contreras                                                                         #
 # Script to get unseen emails from a email account                                                              #
 # Information Extracted from:                                                                                   #
@@ -19,11 +18,36 @@ import threading
 # https://gist.github.com/robulouski/7441883                                                                    #
 #                                                                                                               #
 #################################################################################################################
+    
+ # user data
+userConfig = {
+    "database.user": "",
+    "database.password": ""}
 
-def analizerMail( ):
 
-    user= "esqnqrc.group@gmail.com"  #input("Enter your GMail username:")
-    pwd = ""  #getpass.getpass("Enter your password: ")
+ # load userConfig
+def loadConfig(file, config={}):
+
+    #returns a dictionary with keys of the form
+    #<section>.<option> and the corresponding values
+
+    config = config.copy(  )
+    cp = configparser.ConfigParser(  )
+    cp.read(file)
+    for sec in cp.sections(  ):
+        name = str.lower(sec)
+        for opt in cp.options(sec):
+            config[name + "." + str.lower(opt)] = str.strip(
+                cp.get(sec, opt))
+    return config
+
+
+def analizerMail():
+
+    # get user from userConfig
+    user = loadConfig("config.ini", userConfig)["database.user"]
+    # get password from userConfig
+    pwd = loadConfig("config.ini", userConfig)["database.password"]
 
     # connecting to the gmail imap server
     imapMail = imaplib.IMAP4_SSL("imap.gmail.com")
@@ -31,47 +55,58 @@ def analizerMail( ):
     imapMail.login(user,pwd)
     # List all the mails
 
-    # Idle loop
-    while True:
+    try: 
+        #Idle Loop
+        while True:
 
-        imapMail.list()
+            imapMail.list()
 
-        imapMail.select('Inbox') # here you a can choose a mail box like spam instead
+            imapMail.select('Inbox') # here you a can choose a mail box like INBOX instead
 
-        print("Checking inbox")
+            print("Checking inbox")
 
-        (resp, data) = imapMail.search(None, '(UNSEEN)') # filter unseen mails
+            (resp, data) = imapMail.search(None, '(UNSEEN)') # filter unseen mails
 
-        data = data[0].split() # getting the mails id
+            data = data[0].split() # getting the mails id
 
-        for emailid in data:
+            for emailid in data:
 
-            print("Processing : ")
+                print("Processing : ")
+    
+                (resp, data) = imapMail.fetch(emailid, "(RFC822)") # fetching the mail, "`(RFC822)`" means "get the whole stuff", but you can ask for headers only, etc
+    
+                email_info = data[0][1] # getting the mail content
+    
+                mail = email.message_from_bytes(email_info) # parsing the mail content to get a mail object
+    
+                (resp, data) = imapMail.store(emailid, '-FLAGS', '\\Seen') # Mark email as unseen again (bc fetching brings body of email too)
 
-            resp, data = imapMail.fetch(emailid, "(RFC822)") # fetching the mail, "`(RFC822)`" means "get the whole stuff", but you can ask for headers only, etc
+                #Check if any attachments at all
+                if mail.get_content_maintype() != 'multipart':
+                    continue
 
-            email_info = data[0][1] # getting the mail content
+                print ("["+mail["From"]+"] :" + mail["Subject"] + "\n")
 
-            mail = email.message_from_bytes(email_info) # parsing the mail content to get a mail object
+            time.sleep(10)
 
-            (resp, data) = imapMail.store(emailid, '-FLAGS', '\\Seen') # Mark email as unseen again (bc fetching brings body of email too)
+    finally:        
 
-            #Check if any attachments at all
-            if mail.get_content_maintype() != 'multipart':
-                continue
-
-            print ("["+mail["From"]+"] :" + mail["Subject"] + "\n")
-
-        time.sleep(10)
-
+        imapMail.logout()
 
 
 #####################################################   MAIN  #############################################
 
-threadFunction = threading.Thread(target=analizerMail)
+try:
+    
+    threadFunction = threading.Thread(target=analizerMail)
 
-threadFunction.start()
+    threadFunction.setDaemon(True)
 
-threadFunction.join()
+    threadFunction.start()
 
-imapMail.logout() 
+    threadFunction.join()
+
+except:
+
+    print("\nProgram stopped")
+
